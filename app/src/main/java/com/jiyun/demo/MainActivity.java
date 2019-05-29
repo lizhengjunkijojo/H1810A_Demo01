adc4e926f61429751453c8f05828b4e25731ba2b
lzj <lizhengjunkijojo@163.com>
2019-05-29 15:08:01 +0800

    视频直播模板完成

--- app/src/main/java/com/jiyun/demo/MainActivity.java
+++ app/src/main/java/com/jiyun/demo/MainActivity.java
@@ -1,13 +1,23 @@
 package com.jiyun.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+
+    }
+
+    private void initView() {
+        btn = (TextView) findViewById(R.id.btn);
     }
 }
